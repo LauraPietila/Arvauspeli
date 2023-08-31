e98eff4825e49ebaa80def76c23c7565ef7c9d42
LauraPietila <142221863+LauraPietila@users.noreply.github.com>
2023-08-31 19:56:15 +0300

    Vaihe1

--- src/GitArvaus.java
+++ src/GitArvaus.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
 public class GitArvaus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner in = new Scanner(System.in);
+		
+		String vastaus = "";
+		
+		System.out.println ("Arvaa nimi?");
+		vastaus = in.nextLine();
+		System.out.println (vastaus);
+		
 	}
 
 }
